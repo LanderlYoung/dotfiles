01d18b11220a2cc9689c33f1fb698a07716ed12f
yangchao12 <yangchao12@kuaishou.com>
2024-06-18 16:38:34 +0800

    add telescope config

--- dot-config/nvim/lua/custom/mappings.lua
+++ dot-config/nvim/lua/custom/mappings.lua
@@ -5,14 +5,43 @@ M.dap = {
   n = {
     ["<leader>db"] = { -- db -> debug
       "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line",
+      "Toggle breakpoint at line",
     },
     ["<leader>dr"] = { -- dr -> debugger run
       "<cmd> DapContinue <CR>",
       "Start or continue the debugger",
-    }
+    },
+    ["<leader>dso"] = {
+      "<cmd> DapStepOver <CR>",
+      "Debugger stop over",
+    },
+    ["<leader>dsO"] = {
+      "<cmd> DapStepOut <CR>",
+      "Debugger stop Out",
+    },
+    ["<leader>dsi"] = {
+      "<cmd> DapStepInto <CR>",
+      "Debugger stop into",
+    },
+    ["<leader>dx"] = {
+      "<cmd> DapTerminate <CR>",
+      "Terminate/Exit the debugger",
+    },
   },
 }
 
+M.telescope = {
+  plugin = true, -- for what?
+  n = {
+    ["<leader>o"] = {
+      "<cmd> Telescope lsp_document_symbols <CR>",
+      "Show file symbols",
+    },
+    ["<leader>O"] = {
+      "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>",
+      "Show workspace symbols",
+    }
+  },
+}
 
 return M
