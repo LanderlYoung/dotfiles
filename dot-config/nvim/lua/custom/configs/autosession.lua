697f62a563cf21682449e5ef2284197508b7b02e
yangchao12 <yangchao12@kuaishou.com>
2024-06-22 22:30:11 +0800

    ignore files

---
+++ dot-config/nvim/lua/custom/configs/autosession.lua
@@ -0,0 +1,24 @@
+-- https://github.com/rmagatti/auto-session
+--
+require("auto-session").setup {
+  -- log_level = "error",
+  log_level = "info",
+  auto_session_suppress_dirs = {
+    "~/", "~/Documents", "~/Desktop/", "~/Downloads", "/",
+  },
+
+  session_lens = {
+    buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+    load_on_setup = true,
+    theme_conf = { border = true },
+    previewer = false,
+  },
+}
+
+vim.keymap.set("n", "<leader>sl",
+  require("auto-session.session-lens").search_session, 
+  {
+    noremap = true,
+  }
+)
+
