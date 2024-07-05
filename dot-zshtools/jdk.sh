37fe54709f9acc851aea4b92c19765501b429039
yangchao12 <yangchao12@kuaishou.com>
2024-07-05 11:28:41 +0800

    update

--- dot-zshtools/jdk.sh
+++ dot-zshtools/jdk.sh
@@ -1,6 +1,13 @@
-# User configuration
-if [[ -z $JAVA_HOME && -x /usr/libexec/java_home ]]; then
+if [[ $(uname) == "Darwin" ]]; then
+
+  # User configuration
+  if [[ -z $JAVA_HOME && -x /usr/libexec/java_home ]]; then
     # default to JDK 11
     export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+  fi
+
+  function switch_jdk() { export JAVA_HOME=$(/usr/libexec/java_home -v $1); echo $JAVA_HOME }
+
 fi
-function switch_jdk() { export JAVA_HOME=$(/usr/libexec/java_home -v $1); echo $JAVA_HOME }
+
+# TODO: for linux
