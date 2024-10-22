e1f85d7fc84153c538022d4c4dca9f10593e117b
landerlyoung <landerlyoung@gmail.com>
2024-10-22 11:02:54 +0800

    add neovim rust config

--- dot-zshtools/clash_proxy.sh
+++ dot-zshtools/clash_proxy.sh
@@ -1,11 +1,19 @@
 
 # check Clash Verge reachablility
 CLASH_PROXY_PORT=7897
-if nc -z localhost $CLASH_PROXY_PORT 2> /dev/null; then
-  # consist clash port
+
+function proxy_setup() {
   export https_proxy=http://localhost:$CLASH_PROXY_PORT \
-    http_proxy=http://localhost:$CLASH_PROXY_PORT \
-    all_proxy=socks5://localhost:$CLASH_PROXY_PORT
+      http_proxy=http://localhost:$CLASH_PROXY_PORT \
+      all_proxy=socks5://localhost:$CLASH_PROXY_PORT
+}
+
+function proxy_unset() {
+  unset http_proxy https_proxy all_proxy
+}
+
+if nc -z localhost $CLASH_PROXY_PORT 2> /dev/null; then
+  proxy_setup
 fi
 
 unset CLASH_PROXY_PORT
