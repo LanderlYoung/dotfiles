213aeaa520be2784da635221a94f8aeff8e713c6
landerlyoung <landerlyoung@gmail.com>
2024-10-22 11:02:54 +0800

    ignore files

--- dot-config/nvim/lua/custom/plugins.lua
+++ dot-config/nvim/lua/custom/plugins.lua
@@ -58,24 +58,20 @@ local plugins = {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
+      require "custom.configs.nvimdapui"
     end
   },
   {
     -- A library for asynchronous IO in Neovim
     "nvim-neotest/nvim-nio",
-  }
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    config = function()
+      require "custom.configs.autosession"
+    end
+  },
 }
 
 return plugins
